09ef7b31df7dce69f7372d839ec76d42e5978577
lvniqi <lvniqi@gmail.com>
2014-11-16 23:51:06 +0800

    完成demo 0.1

--- dso_demo_graphview/src/main/java/demos/dso_demo_graphview/MainActivity.java
+++ dso_demo_graphview/src/main/java/demos/dso_demo_graphview/MainActivity.java
@@ -1,9 +1,12 @@
 package demos.dso_demo_graphview;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
@@ -15,6 +18,8 @@ import com.jjoe64.graphview.GraphViewSeries;
 import com.jjoe64.graphview.GraphViewStyle;
 import com.jjoe64.graphview.LineGraphView;
 
+import java.net.MulticastSocket;
+
 
 public class MainActivity extends Activity {
 
@@ -22,41 +27,93 @@ public class MainActivity extends Activity {
     public GraphView lineview;
     static  boolean temp_flag = true;
     Handler handler = new Handler();
-    private Runnable myRunnable = new Runnable() {
+    static double j = 0;
+    //测试用
+    UdpService udpservice;
+    public static Handler mHandler;
+    public static final int MSG_SUCCESS = 0;//获取图片成功的标识
+    //WIFI控制器
+    private WifiManager wifiManager;
+    public static WifiManager.MulticastLock mlock;
+
+    /*private Runnable myRunnable = new Runnable() {
         public void run() {
 
             handler.postDelayed(this, 10);
             double x1 = lineview.getViewportSize();
             text_x1.setText("" + x1);
+            double v = 0;
+            j+= 0.01;
+            for (int i = 0; i < num; i++) {
+                v += 0.2;
+
+                data2[i] = new GraphView.GraphViewData(i, Math.sin(v/2+j + 2)-0.2);
+                if (i == 100) {
+                    data2[i] = new GraphView.GraphViewData(i, -1);
+                }
+
+            }
+            series.resetData(data2);
 
         }
-    };
-
+    };*/
+    static final int num = 512;
+    GraphViewSeries series;
+    GraphView.GraphViewData[] data2 = new GraphView.GraphViewData[num];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //udp 接收
+        udpservice = new UdpService(4507);
+        //WIFI控制器 获取服务
+        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        mlock = wifiManager.createMulticastLock("test");
+        mlock.acquire();
+        //服务程序
+        mHandler = new Handler() {
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case MainActivity.MSG_SUCCESS:
+                        Bundle bundle = new Bundle();
+                        bundle = msg.getData();
+                        byte [] temp_byte = bundle.getByteArray("str");
+                        float [] temp_float = ByteArrayFunction.bytesToFloat(temp_byte);
+                        GraphView.GraphViewData[] data = new GraphView.GraphViewData[temp_float.length];
+                        if(temp_float.length != 0) {
+                            for(int i=0;i<temp_float.length;i++){
+                                data[i] = new GraphView.GraphViewData(i,temp_float[i]);
+                            }
+                            series.resetData(data);
+                        }
+                        break;
+                }
+                super.handleMessage(msg);
+            }
+        };
         setContentView(R.layout.activity_main);
         // init example series data
         // draw sin curve
-        int num = 512;
+
         GraphView.GraphViewData[] data = new GraphView.GraphViewData[num];
-        GraphView.GraphViewData[] data2 = new GraphView.GraphViewData[num];
         double v = 0;
         for (int i = 0; i < num; i++) {
             v += 0.2;
             data[i] = new GraphView.GraphViewData(i, Math.sin(v));
-            data2[i] = new GraphView.GraphViewData(i, Math.sin(v + 2));
-            if (i == 100) {
-                data2[i] = new GraphView.GraphViewData(i, -1);
-            }
+            data2[i] = new GraphView.GraphViewData(i, Math.sin(v/2 + 2)-2);
         }
+        GraphViewSeries series1 =new GraphViewSeries(data);
         lineview = new LineGraphView(this, "");
-        GraphViewSeries series = new GraphViewSeries(null, new GraphViewSeries.GraphViewSeriesStyle(Color.rgb(255, 255, 0), 4), data2);
+        //series1.SetShowMaxFlag(true);
+        //series1.SetShowMinFlag(true);
+        series = new GraphViewSeries(null, new GraphViewSeries.GraphViewSeriesStyle(Color.rgb(255, 255, 0), 4), data2);
+        //series.SetShowMaxFlag(true);
+        //series.SetShowMinFlag(true);
+        //series.SetSignCurveFlag(true);
 // add data
-        lineview.addSeries(new GraphViewSeries(data));
+        lineview.addSeries(series1);
         lineview.addSeries(series);
         lineview.setViewPort(40, 40);
-        lineview.setManualYAxisBounds(1, -1);
+        lineview.setManualYAxisBounds(255,0);
         lineview.setScrollable(true);
 
 // optional - activate scaling / zooming
@@ -88,7 +145,9 @@ public class MainActivity extends Activity {
         }
         ;
         lineview.setCustomLabelFormatter(LabelFormatter);
-        myRunnable.run();
+        Thread tReceived = new Thread(udpservice);
+        tReceived.start();
+        //myRunnable.run();
     }
 
     @Override
